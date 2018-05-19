750bceb75ff179ffd8cc496de3a71c57922d7575
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2018-05-19 16:54:17 -0300

    lab6

---
+++ BuscaArquivo/BuscaArquivo/FormMain.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace BuscaArquivo
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtPathOutput = new System.Windows.Forms.RichTextBox();
+            this.txtPathInput = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.label1.Location = new System.Drawing.Point(15, 5);
+            this.label1.Margin = new System.Windows.Forms.Padding(15, 5, 5, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(368, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entre arquivo ou diretório:                                                      " +
+    "                          ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtPathOutput, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtPathInput, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.458333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.625F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.91666F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(393, 480);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // txtPathOutput
+            // 
+            this.txtPathOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPathOutput.Location = new System.Drawing.Point(17, 99);
+            this.txtPathOutput.Margin = new System.Windows.Forms.Padding(17);
+            this.txtPathOutput.Name = "txtPathOutput";
+            this.txtPathOutput.Size = new System.Drawing.Size(359, 364);
+            this.txtPathOutput.TabIndex = 2;
+            this.txtPathOutput.Text = "";
+            // 
+            // txtPathInput
+            // 
+            this.txtPathInput.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.txtPathInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtPathInput.Location = new System.Drawing.Point(15, 46);
+            this.txtPathInput.Margin = new System.Windows.Forms.Padding(15);
+            this.txtPathInput.Name = "txtPathInput";
+            this.txtPathInput.Size = new System.Drawing.Size(363, 20);
+            this.txtPathInput.TabIndex = 1;
+            this.txtPathInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtPathInput_KeyUp);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 480);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "FormMain";
+            this.Text = "Busca de Arquivo";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox txtPathInput;
+        private System.Windows.Forms.RichTextBox txtPathOutput;
+    }
+}
+
---
+++ BuscaArquivo/BuscaArquivo/FormMain.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BuscaArquivo
+{
+    public partial class FormMain : Form
+    {
+        private bool last_is_file;
+        private int last_file_header_size;
+        private string last_file_content;
+        private string last_path;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            this.last_is_file = false;
+            this.last_file_content = "";
+            this.last_path = "";
+        }
+
+        private void txtPathInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            string path = this.last_path = this.txtPathInput.Text;
+            if(e.KeyCode == Keys.Enter && path != "")
+            {
+                string text_output = "";
+                this.txtPathOutput.Clear();
+                if (File.Exists(path))
+                {
+                    this.last_is_file = true;
+
+                    text_output += "Arquivo: " + path + "\n\n";
+                    text_output += "Arquivo criado em: " + File.GetCreationTime(path) + "\n";
+                    text_output += "Arquivo modificado pela última vez em: " + File.GetLastWriteTime(path) + "\n";
+                    text_output += "Arquivo acessado pela última vez em: " + File.GetLastAccessTime(path) + "\n\n";
+
+                    try
+                    {
+                        this.last_file_content = File.ReadAllText(path);
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        MessageBox.Show("Erro de leitura", "Erro ao tentar ler arquivo.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    text_output += "Texto no arquivo: \n\n";
+                    this.last_file_header_size = text_output.Length;
+                    text_output += this.last_file_content;
+
+                    this.txtPathOutput.AppendText(text_output);
+                }
+                else if (Directory.Exists(path))
+                {
+                    this.last_is_file = false;
+
+                    text_output += "Arquivo: " + path + "\n\n";
+                    text_output += "Arquivo criado em: " + Directory.GetCreationTime(path) + "\n";
+                    text_output += "Arquivo modificado pela última vez em: " + Directory.GetLastWriteTime(path) + "\n";
+                    text_output += "Arquivo acessado pela última vez em: " + Directory.GetLastAccessTime(path) + "\n\n";
+
+                    text_output += "Subdiretórios: \n";
+                    foreach (string line in Directory.GetDirectories(path))
+                    {
+                        text_output += line + "\n";
+                    }
+                    text_output += "\nArquivos no diretório: \n";
+                    foreach (string line in Directory.GetFiles(path))
+                    {
+                        text_output += line + "\n";
+                    }
+                    text_output += "\n";
+
+                    this.txtPathOutput.AppendText(text_output);
+                }
+                else
+                {
+                    MessageBox.Show("Arquivo ou diretório não encontrado.", "Caminho incorreto", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            string modifications = this.txtPathOutput.Text.Substring(this.last_file_header_size);
+            if (this.last_is_file && !modifications.Equals(this.last_file_content))
+            {
+                DialogResult dialogResult = MessageBox.Show("Foram detectadas modificações no arquivo. Deseja salvá-las?",
+                                                            "Salvar alterações", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                if(dialogResult == DialogResult.Yes)
+                {
+                    File.WriteAllText(this.last_path, modifications);
+                }
+                else if(dialogResult == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
